0b1dead56d375af613b5b2595a85730fdf22a2ab
Ryan McDougall <rp.mcdougall@gmail.com>
2019-03-14 21:01:14 -0500

    init and some work

---
+++ lib/cards.ex
@@ -0,0 +1,15 @@
+defmodule Cards do
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+end
